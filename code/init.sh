d524d6e661885af1595c5e3bf80feb321396120b
cantux <can.tuksavul@gmail.com>
2016-12-13 00:42:52 +0300

    updated init.sh

--- code/init.sh
+++ code/init.sh
@@ -8,6 +8,10 @@ wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
 bash Miniconda2-latest-Linux-x86_64.sh
 apt-get install mysql-server
 sudo apt-get install libmysqlclient-dev
-
+apt-get install npm
+sudo npm install -g n
+sudo n stable
 
 git clone https://github.com/cantux/Fall2016Swe573cant.git
+cd Fall2016Swe573cant/code/public
+npm install webpack-dev-server rimraf webpack -g
